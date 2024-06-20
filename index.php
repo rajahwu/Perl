169bf098f5caa4ef0d6eb3f97e56197b93ff6f23
rajahwu <vmradford@gmail.com>
2024-06-20 17:19:26 -0400

    build: add tailwind css

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hello Perl!";
+$content = [
+    "welcome to the perl training server",
+    "checkout the webapp",
+    "checkout training apps"
+];
+
+foreach($content as $c) {
+    echo div($c);
+}
+
+function div($content, $classnames = "") {
+    return "<div class=\"" . $classnames . "\">" . $content . "</div>\n";
+}
