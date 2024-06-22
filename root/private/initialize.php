dc82b3d9424d87fe9ef3479e1e9d38aa0487c3f3
rajahwu <vmradford@gmail.com>
2024-06-22 05:41:01 -0400

    build: configure tailwindcss

--- root/private/initialize.php
+++ root/private/initialize.php
@@ -16,16 +16,17 @@ define("ROOT_PATH", dirname(PRIVATE_PATH, 1));
 define("PUBLIC_PATH", ROOT_PATH . '/public');
 define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
+
 // Assign the root URL to a PHP constant
 // * Do not need to include the domain
 // * Use same document root as webserver
 // * Can set a hardcoded value:
 // define("WWW_ROOT", '/~kevinskoglund/chain_gang/public');
-// define("WWW_ROOT", '');
+define("WWW_ROOT", 'apps/Training/perl/');
 // * Can dynamically find everything in URL up to "/public"
-$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
-$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
-define("WWW_ROOT", $doc_root);
+// $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+// $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+// define("WWW_ROOT", $doc_root);
 
 // Include the Composer autoload file
 require_once PROJECT_PATH . '/vendor/autoload.php';
